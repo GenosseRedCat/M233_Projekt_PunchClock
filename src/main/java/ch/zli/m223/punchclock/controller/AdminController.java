51053d7dee96e19ffcbda5eab361ad89e06a9ec0
genosseredcat <namelessemailadress2@gmail.com>
2020-11-16 02:27:38 -0800

    Im AdminController.java wurde eine Beschreibung hinzugefügt.

--- src/main/java/ch/zli/m223/punchclock/controller/AdminController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/AdminController.java
@@ -1,4 +1,14 @@
 package ch.zli.m223.punchclock.controller;
 
-public class AdminController extends UserController {
+/***
+ * This class was created by Jason Banyer
+ * Date: 05.11.2020
+ *
+ * Description of this class:
+ * This class is an extension of the UserController and it serves to modify the same settings of the BenutzerController for the AdminController.
+ *
+ */
+public class AdminController extends BenutzerController {
+
+
 }
