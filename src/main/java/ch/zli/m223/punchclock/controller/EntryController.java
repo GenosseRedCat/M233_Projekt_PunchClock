77fbf866ce0a2392f6594d930a642b02ba379adb
genosseredcat <namelessemailadress2@gmail.com>
2020-11-16 02:12:13 -0800

    Um den Code besser zu vestehen, habe ich Kommentare hingeschrieben.

--- src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
@@ -17,15 +17,50 @@ public class EntryController {
         this.entryService = entryService;
     }
 
-    @GetMapping
+    @GetMapping //Holt Eintrag
     @ResponseStatus(HttpStatus.OK)
     public List<Entry> getAllEntries() {
         return entryService.findAll();
     }
 
-    @PostMapping
+    //erstellt neuen Eintrag
+    @PostMapping //Postet neuen Eintrag
     @ResponseStatus(HttpStatus.CREATED)
     public Entry createEntry(@Valid @RequestBody Entry entry) {
         return entryService.createEntry(entry);
     }
+
+
+
+
+
+
+
+
+
+
+
+    //löscht vorhandenen Eintrag via ID
+    //@RequestMapping(method = RequestMethod.DELETE, value="/entries/{id}")
+
+    @DeleteMapping("/entries/{id}")
+    @ResponseBody
+    public void deleteEntry(@PathVariable Long id) {
+        entryService.deleteEntry(id);
+    }
+/*
+    @DeleteMapping("/entries/{id}")
+    @ResponseBody
+    public void updateEntry(@PathVariable Long id) {
+        entryService.updateEntry(id);
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteEntry(@Valid @PathVariable Entry entry) {
+        entryService.deleteEntry(entry);
+    }*/
+
+
 }
+
