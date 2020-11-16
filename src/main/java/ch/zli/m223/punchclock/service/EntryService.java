f8df8f5c337db4b2882df4f91c58d39ce95ae895
genosseredcat <namelessemailadress2@gmail.com>
2020-11-16 04:05:01 -0800

    Die EntryService Klasse, wurde durch eine Beschreibung und taugliche Überschreibmethode ergänzt.

--- src/main/java/ch/zli/m223/punchclock/service/EntryService.java
+++ src/main/java/ch/zli/m223/punchclock/service/EntryService.java
@@ -6,18 +6,34 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+/***
+ * This class was not created, but modified by Jason Banyer
+ * Date: 05.11.2020
+ *
+ * Description of this class:
+ * This class is the one, which is responsible for creating the entry Repository and making it publicly available.
+ * Additionally this class does the part of modifying the entries in the entry Repository.
+ *
+ * It is designated as a "Service", so Spring knows, that it has to create an instance of this.
+ * To be precise: Spring learns that this is a company application, which may only be instated once. (Singleton pattern)
+ */
+
 @Service
 public class EntryService {
     private EntryRepository entryRepository;
 
+    //Makes the Entryrepository public.
     public EntryService(EntryRepository entryRepository) {
         this.entryRepository = entryRepository;
     }
 
+    //Creates new Entry
     public Entry createEntry(Entry entry) {
         return entryRepository.saveAndFlush(entry);
     }
 
+    //Finds the List of Entries and returns it.
     public List<Entry> findAll() {
         return entryRepository.findAll();
     }
@@ -27,6 +43,16 @@ public class EntryService {
     public void deleteEntry(Long id) {
         entryRepository.deleteById(id);
     }
+
+    //Overwrites the pre-existing by actually deleting it and making a new one
+    public void updateEntry(Long id, Entry entry) {
+        if(entryRepository.equals(id)){
+            entryRepository.deleteById(id);
+
+            entryRepository.saveAndFlush(entry);
+        }
+    }
+
 }
 
 
