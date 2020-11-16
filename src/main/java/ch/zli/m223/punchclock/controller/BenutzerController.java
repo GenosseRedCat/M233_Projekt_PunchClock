5f10db11617b95c36dd16d084198ec795db2e8c6
genosseredcat <namelessemailadress2@gmail.com>
2020-11-16 05:04:32 -0800

    Eine neue Funktion, welche erlaubt alle Benutzer per URL anzusehen. Zusätzlich wurde die Liste der Benutzer ergänzt.

--- src/main/java/ch/zli/m223/punchclock/controller/BenutzerController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/BenutzerController.java
@@ -1,9 +1,7 @@
 package ch.zli.m223.punchclock.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,18 +23,41 @@ public class BenutzerController {
     private static final List<Benutzer> BENUTZER = Arrays.asList(
             new Benutzer(1, "Thomas"),
             new Benutzer(2, "Stella"),
-            new Benutzer(3, "Tobias")
+            new Benutzer(3, "Tobias"),
+            new Benutzer(4, "Sarah"),
+            new Benutzer(5, "Lina"),
+            new Benutzer(6, "Lora"),
+            new Benutzer(7, "Adrian"),
+            new Benutzer(8, "Bob"),
+            new Benutzer(9, "Marvin"),
+            new Benutzer(10, "Tristan"),
+            new Benutzer(11, "Sebastian"),
+            new Benutzer(12, "Robin"),
+            new Benutzer(13, "Laura"),
+            new Benutzer(14, "Gina"),
+            new Benutzer(15, "Michael"),
+            new Benutzer(16, "Olga"),
+            new Benutzer(17, "Tamara"),
+            new Benutzer(18, "Stefan"),
+            new Benutzer(19, "Valentin")
     );
-    @GetMapping(path = "/{benutzerId}")
-    public Benutzer getBenutzer(@PathVariable("benutzerId") Integer benutzerId){
-        return BENUTZER.stream()
-                .filter(benutzer -> benutzerId.equals(benutzer.getBenutzerId()))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Benutzer: " + benutzerId + " existiert nicht"));
 
-
+    public static List<Benutzer> getAllBENUTZER() {
+        return BENUTZER;
     }
 
+    @GetMapping(path = "/{benutzerId}")
+    public Benutzer getBenutzer(@PathVariable("benutzerId") Integer benutzerId) {
 
+        return BENUTZER.stream()
+                    .filter(benutzer -> benutzerId.equals(benutzer.getBenutzerId()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("Benutzer: " + benutzerId + " existiert nicht"));
+        }
+    @RequestMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Benutzer> getEveryBenutzer() {
 
+        return BenutzerController.getAllBENUTZER();
+    }
 }
